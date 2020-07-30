5fe0a9edea22f10377723867d1a8444bceb2f4cb
seiyakidokoro <seiya.kidokoro@gmail.com>
2020-07-30 18:38:30 +0900

    ルーティング

--- my-laravel-app/routes/web.php
+++ my-laravel-app/routes/web.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// タスクダッシュボード表示
+Route::get('/task','TaskController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 新規タスク作成
+Route::post('/task','TaskController@store');
+
+// 詳細
+Route::get('/task/{task}/show','TaskController@show');
+
+// 編集
+Route::get('/task/{task}','TaskController@edit')->name('article_edit');
+
+// タスク更新
+Route::put('/task/{task}','TaskController@update');
+
+// タスク削除
+Route::post('/task/{task}/delete','TaskController@destroy')->name('article_delete');
+
