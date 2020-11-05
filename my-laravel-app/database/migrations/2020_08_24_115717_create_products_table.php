82e58c6d9835feccb748dbe2d59e4ca4f71ffdeb
中村尚央 <nakamura8nao@gmail.com>
2020-11-05 14:24:22 +0900

    productsテーブル作成 migration

---
+++ my-laravel-app/database/migrations/2020_08_24_115717_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->text('description')->nullable();
+			$table->integer('point');
+			$table->string('image');
+			$table->softDeletes();
+			$table->timestamp('updated_at')->useCurrent();
+			$table->timestamp('created_at')->useCurrent();
+
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
